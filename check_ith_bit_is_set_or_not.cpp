a85e19c72baab4b8c78fffb1390aeeac217da2c5
ShivamDude121 <97700743+ShivamDude121@users.noreply.github.com>
2024-09-02 02:49:49 +0530

    Create check_ith_bit_is_set_or_not.cpp

---
+++ check_ith_bit_is_set_or_not.cpp
@@ -0,0 +1,15 @@
+
+
+// here indexing is on zero basis i 0---->>n-1
+
+bool isKthBitSet(int n, int i){
+    
+    int mask = 1 << (k);
+
+
+     if(mash&n)
+       retrun true;
+
+     return false
+    
+}
